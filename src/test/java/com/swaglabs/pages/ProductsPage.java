09039de32b816c5b88ac0d0659c64601828060e1
rajakumarsingh1857 <96594157+rajakumarsingh1857@users.noreply.github.com>
2026-03-25 15:16:13 +0530

    Add ProductsPage.java with product listing and sorting functionality

---
+++ src/test/java/com/swaglabs/pages/ProductsPage.java
@@ -0,0 +1,58 @@
+package com.swaglabs.pages;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ProductsPage {
+    private List<Product> products;
+
+    public ProductsPage() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void sortProductsByName() {
+        Collections.sort(products, new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().compareTo(p2.getName());
+            }
+        });
+    }
+
+    public void sortProductsByPrice() {
+        Collections.sort(products, new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return Double.compare(p1.getPrice(), p2.getPrice());
+            }
+        });
+    }
+}
+
+class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
