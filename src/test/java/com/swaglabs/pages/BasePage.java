39103681e655d5c70ab28914f94e0930be0dbd50
rajakumarsingh1857 <96594157+rajakumarsingh1857@users.noreply.github.com>
2026-03-25 15:16:24 +0530

    Create BasePage.java with WebDriver wait utilities and common element interaction methods

---
+++ src/test/java/com/swaglabs/pages/BasePage.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    // Common element interaction methods
+    protected WebElement waitForElementToBeVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitForElementToBeClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected void clickElement(By locator) {
+        waitForElementToBeClickable(locator).click();
+    }
+
+    protected void inputText(By locator, String text) {
+        WebElement element = waitForElementToBeVisible(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+}
