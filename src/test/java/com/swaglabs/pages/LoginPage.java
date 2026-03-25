8d1e1ffd838e475f4f0f71299813185f202bef23
rajakumarsingh1857 <96594157+rajakumarsingh1857@users.noreply.github.com>
2026-03-25 15:16:18 +0530

    Create LoginPage.java with login page elements and methods

---
+++ src/test/java/com/swaglabs/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package com.swaglabs.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    // Page elements
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // Methods
+    public void enterUsername(String username) {
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        loginButton.click();
+    }
+}
